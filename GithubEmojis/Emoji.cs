9304b383bef0776703798c09f111e48d6b17766e
vgmda <vasilegancin@gmail.com>
2022-06-03 22:13:20 +0100

    GetEmojis()

---
+++ GithubEmojis/Emoji.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace GithubEmojis;
+
+public class Emoji
+{
+    public string Key { get; set; }
+    public string Url { get; set; }
+}
+
---
+++ GithubEmojis/GithubEmojiService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace GithubEmojis;
+
+public class GithubEmojiService
+{
+    const string GithubEmojiUrl = "https://api.github.com/emojis";
+    private readonly HttpClient _httpClient;
+    private IList<Emoji> _emojis;
+
+    public GithubEmojiService()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "vgmda-github-emojis");
+    }
+
+    public async Task<IList<Emoji>> GetEmojis()
+    {
+        if (_emojis == null || _emojis.Count <= 0)
+        {
+            var emojiStr = await _httpClient.GetStringAsync(GithubEmojiUrl);
+            try
+            {
+                // TODO - GetEmojisFrom() method
+                _emojis = GetEmojisFrom(emojiStr);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"error: {ex.ToString()}");
+            }
+
+        }
+
+
+        return _emojis;
+    }
+
+}
+
